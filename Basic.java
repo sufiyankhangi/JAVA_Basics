135f96fa35d64894b9acf7acb7d158ca98a8ba93
Sufiyan <75123948+sufiyankhangi@users.noreply.github.com>
2021-12-23 14:46:48 +0530

    Add files via upload

---
+++ Basic.java
@@ -0,0 +1,93 @@
+import java.util.*;
+public class Basic {
+    public static void main(String[] args) {
+        // half pyramid with numbers
+        // for(int i=1;i<=5;i++){
+        //     for(int j =1;j<=i;j++){
+        //         System.out.print(j);
+        //     }System.out.println();
+        // }
+
+        // 1
+        // 12
+        // 123
+        // 1234
+        // 12345
+        
+        
+        // inverted half pyramid
+        // for(int i=1; i<=4;i++){
+        //     for(int n =4 ; n-i >0;n--){
+        //         System.out.print("*");
+        //     }
+        //     for (int j=1; j<=i;j++){
+        //         System.out.print("^");
+        //     }System.out.println();
+        // }
+
+        // ***^
+        // **^^
+        // *^^^
+        // ^^^^
+
+
+        // holow rectangle
+        // int n = 4;
+        // int m = 5;
+        // for (int i = 1; i<= n; i++){
+        //     for(int j =1;j<= m;j++){
+        //         if(i==1||j==1||i==4||j==5){
+        //             System.out.print("*");
+        //         }else{
+        //             System.out.print(" ");
+        //         }
+        //     }System.out.println();
+        // }
+
+
+        // inverted tringale
+        // int n = 4;
+        // for (int i = n; i>0; i--){
+        //     for(int j =i;j>0 ;j--){
+        //         System.out.print("*");
+        //     }System.out.println();
+        // }
+
+
+
+
+        // for printing something
+        //System.out.println("hello world");
+
+
+        //for take input form user
+        //Scanner sc = new Scanner(System.in);
+        //String name = sc.nextLine();
+        //System.out.println(name);
+        
+
+        //  if else statments
+        // Scanner sc = new Scanner(System.in);
+        // int age = sc.nextInt();
+
+        // if(age<18){
+        //     System.out.println("not a adult");
+        // }else{
+        //     System.out.println("a adult");
+        // }
+        
+
+        // using of switch and cases
+        // switch( age){
+        //     case 1 : System.out.println(" u press 1");
+        //     break;
+        //     case 2 : System.out.println(" 2 preesd");
+        //     case 3 : System.out.println("3 preesd");
+        //     break;
+        //     case 40 : System.out.println("40 pressed");
+        //     break;
+        //     default : System.out.println("invalid");
+        // }
+
+    }
+}
